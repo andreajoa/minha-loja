90283f3e109109e161b4b71428e5eca4c07a75cd
andreajoa <100246160+andreajoa@users.noreply.github.com>
2026-08-05 11:16:11 -0300

    integra blocos visuais e instagram na homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import HeroCarousel from "@/components/HeroCarousel";
+import HomeReferenceSections, { InstagramFollowStrip } from "@/components/HomeReferenceSections";
 import StorefrontHome from "@/components/StorefrontHome";
 
 export default function Home() {
   return (
     <>
       <HeroCarousel />
+      <HomeReferenceSections />
       <div className="storefront-after-carousel">
         <StorefrontHome />
       </div>
+      <InstagramFollowStrip />
       <style>{`
         .storefront-after-carousel > div > section:first-child {
           display: none;
