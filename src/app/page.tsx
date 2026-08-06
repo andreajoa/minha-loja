b65958903ade095804e761d1579f898801f7dcbd
andreajoa <100246160+andreajoa@users.noreply.github.com>
2026-08-06 15:42:26 -0300

    Simplifica a homepage e remove seções duplicadas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
 import HeroCarousel from "@/components/HeroCarousel";
-import HomeReferenceSections, { InstagramFollowStrip } from "@/components/HomeReferenceSections";
 import StorefrontHome from "@/components/StorefrontHome";
 
 export default function Home() {
   return (
     <>
       <HeroCarousel />
-      <HomeReferenceSections />
-      <div className="storefront-after-carousel">
-        <StorefrontHome />
-      </div>
-      <InstagramFollowStrip />
-      <style>{`
-        .storefront-after-carousel > div > section:first-child {
-          display: none;
-        }
-      `}</style>
+      <StorefrontHome />
     </>
   );
 }
