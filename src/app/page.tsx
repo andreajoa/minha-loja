91ffd027d6f4a2a91f7c01dbb1beb114f3a372d5
andreajoa <100246160+andreajoa@users.noreply.github.com>
2026-08-06 15:49:03 -0300

    Restaura estrutura original da homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import HeroCarousel from "@/components/HeroCarousel";
+import HomeReferenceSections, { InstagramFollowStrip } from "@/components/HomeReferenceSections";
 import StorefrontHome from "@/components/StorefrontHome";
 
 export default function Home() {
   return (
     <>
       <HeroCarousel />
-      <StorefrontHome />
+      <HomeReferenceSections />
+      <div className="storefront-after-carousel">
+        <StorefrontHome />
+      </div>
+      <InstagramFollowStrip />
+      <style>{`
+        .storefront-after-carousel > div > section:first-child {
+          display: none;
+        }
+      `}</style>
     </>
   );
 }
