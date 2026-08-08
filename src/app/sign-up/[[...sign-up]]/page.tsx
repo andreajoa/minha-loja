48b83a09786dc3de2020f676500050bae0580d64
andreajoa <100246160+andreajoa@users.noreply.github.com>
2026-08-07 21:22:09 -0300

    security: bloqueia cadastro público no app

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,7 @@
+import { notFound } from "next/navigation";
+
+export const metadata = { robots: { index: false, follow: false } };
+
+export default function DisabledSignUpPage() {
+  notFound();
+}
